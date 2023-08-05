e8e3b67cf0a80cb0d2a29e4875d804a88dc46865
miyama <36231225+hige-dev@users.noreply.github.com>
2023-08-06 06:00:37 +0900

    fix database.py

--- app/db/database.py
+++ app/db/database.py
@@ -3,41 +3,48 @@ from configparser import ConfigParser
 
 class Database:
     def __init__(self, filename='db/dbconfig.ini', section='postgresql'):
-        self.dbconf = self.__config(filename, section)
+        self.__config(filename, section)
 
     def __connect(self):
-        conn = None
-        try:
-            conn = psycopg2.connect(**self.dbconf)
-            conn.set_client_encoding('utf-8')
-            return conn
-        except (Exception, psycopg2.DatabaseError) as e:
-            print(e)
-        return conn
+        conn = psycopg2.connect(**self.dbconf)
+        conn.set_client_encoding('utf-8')
+        self.__connection = conn
+        self.__cursor = conn.cursor()
+
+    def __close(self):
+        self.__cursor.close()
+        self.__connection.close()
 
     def __config(self, filename, section):
         parser = ConfigParser()
         parser.read(filename)
 
-        dbconf = {}
+        self.dbconf = {}
         if parser.has_section(section):
             params = parser.items(section)
             for param in params:
-                dbconf[param[0]] = param[1]
+                self.dbconf[param[0]] = param[1]
         else:
             raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-        return dbconf
 
-    def execute(self, sql):
-        conn = self.__connect()
-        cur = conn.cursor()
+    def __execute(self, sql):
         try:
-            cur.execute(sql)
-            conn.commit()
-            res = cur.fetchall()
+            self.__cursor.execute(sql)
+            self.__connection.commit()
         except psycopg2.Error as e:
             print(e)
-            conn.rollback()
-        cur.close()
-        conn.close()
+            self.__connection.rollback()
+
+    def select_all(self):
+        self.__connect()
+        sql = 'select * from users'
+        self.__execute(sql)
+        res = self.__cursor.fetchall()
+        self.__close()
         return res
+
+    def insert_user(self, name):
+        self.__connect()
+        sql = f'insert into users (name) values (\'{name}\')'
+        self.execute(sql)
+        self.__close()

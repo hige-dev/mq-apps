0a53ac85f9a90837f11aa9884fd59e836cec559e
miyama <36231225+hige-dev@users.noreply.github.com>
2023-08-08 06:37:16 +0900

    fix mq

--- app/mq/mq.py
+++ app/mq/mq.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python
 import pika
-import db.database as db
+import db.database as dba
 
 class RabbitMQ():
     def __init__(self):
         self.cred = pika.PlainCredentials('root', 'password')
 
-    def connection(self):
+    def __connection(self):
         return pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', 5672, '/', self.cred))
 
     def publish(self, exchange='', routing_key='', body=''):
-        conn = self.connection()
+        conn = self.__connection()
         channel = conn.channel()
         channel.queue_declare(queue=routing_key)
         channel.basic_publish(exchange=exchange,routing_key=routing_key,body=body)
         conn.close()
 
     def callback(self, ch, method, properties, body):
-        print("Received: '{}'".format(body))
-        self.__db_query_exec(body)
+        sql = body.decode()
+        print(f"Received: '{sql}'")
+        self.__db_query_exec(sql)
         ch.basic_ack(delivery_tag = method.delivery_tag)
 
-    def __db_query_exec(self, body):
-        db = db.Database()
-        db.execute(body)
+    def __db_query_exec(self, sql):
+        db = dba.Database()
+        db.execute(sql)
 
     def consume(self):
-        conn = self.connection()
+        conn = self.__connection()
         channel = conn.channel()
 
         channel.basic_consume(
             queue = 'app',
-            on_message_callback=self.callback
+            on_message_callback=self.callback,
+            auto_ack=True
         )
 
         channel.start_consuming()
